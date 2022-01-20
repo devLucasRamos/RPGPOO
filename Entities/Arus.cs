1f0b1cf5d6322f0a3b81cac88eb724ed418034d6
devLucasRamos <lucas2adriano@gmail.com>
2022-01-20 18:28:29 -0300

    Criado o obj herói e colocando metodos e propriedades

---
+++ Entities/Arus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGPOO.Entities
+{
+    internal class Arus
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public string HeroType { get; set; }
+
+        public Arus(string name, int level, string heroType)
+        {
+            Name = name;
+            Level = level;
+            HeroType = heroType;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RPGPOO.Entities;
+
+Arus hero = new Arus("Arus",23,"Knight");
+
+Console.WriteLine(hero);
